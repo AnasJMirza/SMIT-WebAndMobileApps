307f00a60d91fcd007ba017384e894ac4e23ee7b
AnasJMirza <ajmhimself@gmail.com>
2021-12-21 17:08:02 +0500

    Four Question Done

--- Week-08 (JS)/JS-02/script.js
+++ Week-08 (JS)/JS-02/script.js
@@ -229,6 +229,24 @@ function monthName(input) {
 monthName("10/11/2009");
 monthName("11/13/2014");
 
+// ----------------------------------------------------------------------------------------
+
+// Write a JavaScript function to compare dates (i.e. greater than, less than or equal to).   
+
+// Test Data :
+// console.log(compare_dates(new Date('11/14/2013 00:00'), new Date('11/14/2013 00:00')));
+// console.log(compare_dates(new Date('11/14/2013 00:01'), new Date('11/14/2013 00:00')));
+// console.log(compare_dates(new Date('11/14/2013 00:00'), new Date('11/14/2013 00:01')));
+// Output :
+// "Date1 = Date2"
+// "Date1 > Date2"
+// "Date2 > Date1"
+
+
+
+
+
+
 
 
 
