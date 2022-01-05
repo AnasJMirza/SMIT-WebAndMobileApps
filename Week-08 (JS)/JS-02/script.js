ed9bd2a15523d16dee795f2122b849168568a701
AnasJMirza <ajmhimself@gmail.com>
2022-01-05 16:56:09 +0500

    Question 8 started

--- Week-08 (JS)/JS-02/script.js
+++ Week-08 (JS)/JS-02/script.js
@@ -251,6 +251,25 @@ console.log(isWeekEnd('Nov 16, 2014'));
 console.log(isWeekEnd('Nov 17, 2014'));
 
 
+// Q - 09 : Write a JavaScript function to get difference between two dates in days.   
+
+// Test Data :
+// console.log(date_diff_indays('04/02/2014', '11/04/2014'));
+// console.log(date_diff_indays('12/02/2014', '11/04/2014'));
+// Output :
+// 216
+// -28
+
+
+var dateDiffInDays = (date1, date2) => {
+    var date = new Date(date1);
+    console.log(date);
+}
+
+dateDiffInDays('04/02/2014', '11/04/2014');
+
+
+
 
 
 
