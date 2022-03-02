c446dd0e9d49ca2223ef6b7fc958260828253b60
AnasJMirza <ajmhimself@gmail.com>
2022-03-02 20:55:36 +0500

    works on sidebar

--- Week-20 (MS TODO)/ms-todo/src/components/sidebar/Sidebar.js
+++ Week-20 (MS TODO)/ms-todo/src/components/sidebar/Sidebar.js
@@ -6,6 +6,11 @@ import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import HomeIcon from '@mui/icons-material/Home';
 import MenuIcon from '@mui/icons-material/Menu';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import PeopleIcon from '@mui/icons-material/People';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import DoneOutlineIcon from '@mui/icons-material/DoneOutline';
+
 
 const SideBar = () => {
     return (
@@ -13,11 +18,11 @@ const SideBar = () => {
             <div className='sidebar'>
 
                 <span className='menuIcon'>
-                    <MenuIcon />
+                    <MenuIcon fontSize='small'/>
                 </span>
                 <div className='sidebarHorizentalAlign'>
                     
-                    <LightModeIcon/>
+                    <LightModeIcon fontSize='small'/>
 
                     <div className='myDayText'>
                         <p className='sidebarHorizentalPadding'>My Day</p>
@@ -26,7 +31,7 @@ const SideBar = () => {
 
                 <div className='sidebarHorizentalAlign'>
 
-                    <StarBorderIcon/>
+                    <StarBorderIcon fontSize='small'/>
 
                     <div className='importentText'>
                         <p className='sidebarHorizentalPadding'>Importent</p>
@@ -35,7 +40,7 @@ const SideBar = () => {
 
                 <div className='sidebarHorizentalAlign'>
 
-                    <CalendarTodayIcon/>
+                    <CalendarTodayIcon fontSize='small'/>
 
                     <div className='plannedText'>
                         <p className='sidebarHorizentalPadding'>Planned</p>
@@ -44,7 +49,7 @@ const SideBar = () => {
 
                 <div className='sidebarHorizentalAlign'>
 
-                    <AccountCircleIcon/>
+                    <AccountCircleIcon fontSize='small'/>
 
                     <div className='assignedText'>
                         <p className='sidebarHorizentalPadding'>Assigned to me</p>
@@ -53,15 +58,44 @@ const SideBar = () => {
 
                 <div className='sidebarHorizentalAlign'>
 
-                    <HomeIcon/>
+                    <HomeIcon fontSize='small'/>
 
                     <div className='tasksText'>
                         <p className='sidebarHorizentalPadding'>Tasks</p>
                     </div>
 
                 </div>
+
+
+                {/* Bottom Icons */}
+
+                <div className='bottomIcons'>
+                    <div className='bottomIcon'>
+                        <MailOutlineIcon fontSize='small'/>
+                    </div>
+
+                    <div className='bottomIcon'>
+                        <CalendarTodayIcon fontSize='small'/>
+                    </div>
+
+                    <div className='bottomIcon'>
+                        <PeopleIcon fontSize='small'/>
+                    </div>
+
+                    <div className='bottomIcon'>
+                        <AttachFileIcon fontSize='small'/>
+                    </div>
+
+                    <div className='bottomIcon'>
+                        <DoneOutlineIcon fontSize='small'/>
+                    </div>
+
+                </div>
             </div>
+
         </div>
+
+            
     );
 };
 
