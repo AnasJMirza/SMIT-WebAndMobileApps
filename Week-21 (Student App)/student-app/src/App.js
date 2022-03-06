60e94ca5a71845283f461265d46da575d528227a
AnasJMirza <ajmhimself@gmail.com>
2022-03-07 00:33:07 +0500

    works on form

--- Week-21 (Student App)/student-app/src/App.js
+++ Week-21 (Student App)/student-app/src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [name, setName] = useState('')
+  console.log(name);
+
   return (
     <div>
       
+      {/* Form Section */}
+      <input type="text"  onChange={(e)=>setName(e.target.value)}  placeholder='Enter your name'/>
+
+      <p>{name}</p>
     </div>
   );
 };
