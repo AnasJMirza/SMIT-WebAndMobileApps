e3e39fa20389ea09c4fbb6ed2cd3bc89f078faa9
AnasJMirza <ajmhimself@gmail.com>
2022-03-07 00:21:42 +0500

    Update README + some work

--- Week-21 (Student App)/student-app/src/App.js
+++ Week-21 (Student App)/student-app/src/App.js
@@ -1,82 +1,11 @@
-import './App.css';
-import { Grid } from '@mui/material';
-import { useState } from 'react';
+import React from 'react';
 
-
-function App() {
-
-  const data = [
-    {name : 'Anas J Mirza', rollNo : 14148, age : 22, degree : 'BSCS'},
-    {name : 'Umar', rollNo : 12228, age : 19, degree : 'BSSE'},
-    {name : 'ali', rollNo : 12138, age : 20, degree : 'BCA'},
-  ]
- 
-
-
-  
-
-  const [name, setName] = useState('');
-
-  const submitHandler = () =>{
-    return null
-  }
-
+const App = () => {
   return (
     <div>
-
-      <Grid container>
-
-        {/* Form Section */}
-
-        <Grid item md={6}>
-          <form>
-
-            Name : &nbsp; &nbsp;  &nbsp; <input  className='input' type={"text"} placeholder="Enter your Name" onChange={(e)=>{setName(e.target.value)}}/><br/>
-
-            Roll No. : &nbsp;&nbsp; <input className='input' type={"number"} placeholder="Enter your Roll Number"/> <br/>
-
-            Age : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input className='input' type={"number"} placeholder="Enter your Age"/><br/>
-
-            Degree : &nbsp;&nbsp;&nbsp; <input className='input' type={"text"} placeholder="Enter your Degree"/><br/>
-
-          </form>
-
-          <div className='submit' onClick={submitHandler}>Submit</div>
-        </Grid>
-
-
-
-
-
-        {/* Table Section */}
-
-        <Grid item md={6}>
-
-          <table border="border" width={"70%"} >
-            <tr>
-              <th>Name</th>  
-              <th>Roll Number</th>  
-              <th>Age</th>  
-              <th>Degree</th>
-            </tr> 
-              
-                {data.map((item)=>{
-                  return (<tr>{item.name}</tr>)
-                })}
-
-                {data.map((item)=>{
-                  return (<tr>{item.rollNo}</tr>)
-                })}
-            
-
-          </table>
-
-        </Grid>
-
-      </Grid>
-
+      
     </div>
   );
-}
+};
 
 export default App;
