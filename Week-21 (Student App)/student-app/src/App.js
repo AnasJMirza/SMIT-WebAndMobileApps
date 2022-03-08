e8c80bbbe77f9a8a0a2d60fda7c7a33d9e4603e5
AnasJMirza <ajmhimself@gmail.com>
2022-03-08 22:22:20 +0500

    wroks on CREATE functionality

--- Week-21 (Student App)/student-app/src/App.js
+++ Week-21 (Student App)/student-app/src/App.js
@@ -1,16 +1,48 @@
 import React, { useState } from 'react';
+import './App.css'
+import Button from '@mui/material/Button';
 
 const App = () => {
+
+  const data = [
+    {name : 'Anas', age : 22, rollNo : 14148},
+    {name : 'Imran', age : 18, rollNo : 14234},
+    {name : 'Junaid', age : 21, rollNo : 12238},
+  ]
+
+  const [studentsData, setStudentsData] = useState(data)
+
   const [name, setName] = useState('')
-  console.log(name);
+
 
   return (
     <div>
       
       {/* Form Section */}
+
       <input type="text"  onChange={(e)=>setName(e.target.value)}  placeholder='Enter your name'/>
 
-      <p>{name}</p>
+      <div className='btnWrapper'>
+      <Button variant="contained">Submit</Button>
+      </div>
+      
+
+      {/* Data Visualization Section */}
+
+      <table className='table' border='border'>
+
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Roll No</th>
+        </tr>
+
+        {studentsData.map((item)=>{
+          return <tr> <td>{item.name}</td> <td>{item.age}</td> <td>{item.rollNo}</td> </tr>
+        })}
+
+      </table>
+
     </div>
   );
 };
