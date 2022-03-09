6fbf4fe9fa5dd21d0416717f5fb55c8b20309a01
AnasJMirza <ajmhimself@gmail.com>
2022-03-09 19:55:55 +0500

    works on CREATE functionality

--- Week-21 (Student App)/student-app/src/App.js
+++ Week-21 (Student App)/student-app/src/App.js
@@ -2,32 +2,39 @@ import React, { useState } from 'react';
 import './App.css'
 import Button from '@mui/material/Button';
 
+const data = [
+  {name : 'Anas', age : 22, rollNo : 14148},
+  {name : 'Imran', age : 18, rollNo : 14234},
+  {name : 'Junaid', age : 21, rollNo : 12238},
+]
+
 const App = () => {
 
-  const data = [
-    {name : 'Anas', age : 22, rollNo : 14148},
-    {name : 'Imran', age : 18, rollNo : 14234},
-    {name : 'Junaid', age : 21, rollNo : 12238},
-  ]
+  
 
   const [studentsData, setStudentsData] = useState(data)
 
   const [name, setName] = useState('')
   const [age, setAge] = useState('')
   const [rollNo, setRollNo] = useState('')
-
-  const newData = {
-    name : name,
-    age : age,
-    rollNo : rollNo
-  }
-
-  
   
 
   const submitHandler = ()=>{
+    
+
+    let newData = {
+      name : name,
+      age : age,
+      rollNo : rollNo
+    }
+
     setStudentsData([...data, newData]);
-    console.log(studentsData);
+    
+    // setName('');
+    // setAge('');
+    // setRollNo('');
+
+
   }
 
 
