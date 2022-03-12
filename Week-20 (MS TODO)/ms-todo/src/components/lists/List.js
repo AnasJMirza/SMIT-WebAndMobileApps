b14e2b84bcd1990ac47c8fc22302e6dc4b0d5495
AnasJMirza <ajmhimself@gmail.com>
2022-03-12 15:51:21 +0500

    works on table

--- Week-20 (MS TODO)/ms-todo/src/components/lists/List.js
+++ Week-20 (MS TODO)/ms-todo/src/components/lists/List.js
@@ -1,16 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './List.css'
 
+let data = ["Some work"]
+
 const List = () => {
+
+    const [inputValue, setInputValue] = useState('')
+    const [todos, setTodos] = useState([data])
+
+    const submitHandler = ()=>{
+
+        let newTodo = inputValue
+        setTodos([...todos, newTodo])
+    }
+
+    // const enter = (e)=>{
+    //     if (e.keyCode === 13) {
+    //         // Cancel the default action, if needed
+    //         e.preventDefault();
+    //         // Trigger the button element with a click
+    //         click();
+    //     }
+    // }
+
+
     return (
         <div>
+            {/* Add todo Section to get all the values enter by user */}
+
             <div className='lists'>
                 <div className='addTodo'>
                     <div className='textWrapper'>
-                        <textarea className='text' rows={2} cols={200} placeholder="Add a Task" />
+                        <input type='text' placeholder='Add Some Task' onChange={(e)=>{setInputValue(e.target.value)}} className='addTask' />
                     </div>
+
+                    <button onClick={submitHandler}>submit</button>
                 </div>
             </div>
+
+            {/* Table in which the data stored */}
+
+            <div className='tableSection'>
+                <table className='table'>
+                    {todos.map((item)=>{
+                        return <tr><td>{item}</td></tr>
+                    })}
+
+                    {/* <tr>
+                        <td>anas</td>
+                    </tr> */}
+                </table>
+            </div>
+            
         </div>
     );
 };
