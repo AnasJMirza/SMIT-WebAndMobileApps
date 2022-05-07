5772dcf0c56ba4818029aca6e1dc36016b511083
AnasJMirza <ajmhimself@gmail.com>
2022-05-07 07:51:51 +0500

    works on add style

--- Week-28-30 (Full Student App-Backend)/studentapp-firebase/src/pages/form/Form.js
+++ Week-28-30 (Full Student App-Backend)/studentapp-firebase/src/pages/form/Form.js
@@ -9,11 +9,17 @@ const Form = () => {
     const { nameGetter, rollNumGetter, degreeGetter, submitHandler } = UseForm();
     
     return (
-        <div  className='form-body'>
-            <form action='#' className='form-bg'>
-                <input type={"text"} placeholder="Name" onChange={(e)=>nameGetter(e)} />
-                <input type={"number"} placeholder="Roll Number" onChange={(e)=>rollNumGetter(e)} />
-                <input type={"text"} placeholder="Degree" onChange={(e)=>degreeGetter(e)} />
+        <div  className='main-body'>
+            <form action='#' className='form-body'>
+                <div className='add-students'>
+                    Add Students
+                    <hr/>
+                </div>
+                <div className='input-feilds'>
+                    <input type={"text"} placeholder="Name" onChange={(e)=>nameGetter(e)}  className="input"/>
+                    <input type={"number"} placeholder="Roll Number" onChange={(e)=>rollNumGetter(e)}  className="input"/>
+                    <input type={"text"} placeholder="Degree" onChange={(e)=>degreeGetter(e)}  className="input"/>
+                </div>
 
                 <div onClick={submitHandler}>
                 <Button label="Submit" bg="crimson" color="White" route="/" />
